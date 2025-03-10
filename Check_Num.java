1e73892de55371b34ef04a1777fbb5dae27d4b72
Spandana66 <macharalaspandana1999@gmail.com>
2025-03-10 18:27:18 +0530

    Add files via upload

---
+++ Check_Num.java
@@ -0,0 +1,19 @@
+class Check_Num 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Program Start");
+		int a = 2;
+		if(40%a==0)
+		{
+		  --a;
+		System.out.println(a);
+	}
+	else
+	{
+	 a++;
+	System.out.println(a);
+	}
+	System.out.println("Program Ends");
+	}
+	}
